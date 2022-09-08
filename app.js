db749781b9255a17ac62590041eeedca0bc91837
salom12345677 <qilichevasliddin93@gmail.com>
2022-09-08 13:02:02 +0500

    finished example

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+//  Alisher 
+
+// const plane = 500;
+// const hotel = 250;
+// const travel = 120;
+// const dollor = 10650.34;
+// const yevro = 11650.03;
+// const price = (plane+hotel)*dollor;
+// const price2 = travel*yevro
+// const totalAmount = price+price2
+// alert('Umumiy summa '+ totalAmount + " so'm")
+
+
+
+// 1-misol
+
+// const num = 10
+// const num2 = 5
+// console.log(num+num2)
+
+
+// 2-misol
+
+// const num1 =25
+// const num2 =15
+// const result = num1-num2
+// console.log(result)
+
+
+
+// 3-misol
+
+
+// const a = 10
+// const b = 5
+// const c = a-b
+// const d = 7
+// const result = c+d
+// console.log(result)
+
+
+// 4-misol
+
+// const a = 10
+// const b = 3
+// console.log(a%b)
+
+
+// 5-misol
+
+
+// const result = Math.pow(2,10)
+// console.log(result)
+
